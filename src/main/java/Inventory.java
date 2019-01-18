6ddcf592637b74a29ab979a5355b14857d3b62aa
Mina Lee <mina849669@gmail.com>
2019-01-17 23:34:32 -0800

    Changed member Set<Product> products

--- src/main/java/Inventory.java
+++ src/main/java/Inventory.java
@@ -1,5 +1,7 @@
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 class InsufficientInventory extends Exception {
     public InsufficientInventory(int currentInventory, int requestedInventory) {
@@ -9,58 +11,81 @@ class InsufficientInventory extends Exception {
 }
 
 public class Inventory {
-    private List<Product> products = new ArrayList<>();
+    private Set<Product> products = new HashSet<>();
 
-    private int getProductIndex(String productId) {
-        for (int i = 0; i < products.size(); i++) {
-            if (products.get(i).getProductId().equals(productId)) {
-                return i;
-            }
-        }
 
-        return -1;
-    }
 
     void addProduct(String productId, double price, int quantity) {
-        int index = getProductIndex(productId);
-        if (index >= 0) {
-            Product product = products.get(index);
+        if(inStock(productId)) {
+            Product product = getProduct(productId);
             product.addStock(quantity);
             if (product.getPrice() != price) {
                 product.setPrice(price);
             }
-
         } else {
             products.add(new Product(productId, price, quantity));
         }
+//        int index = getProductIndex(productId);
+//        if (index >= 0) {
+//            Product product = products.get(index);
+//            product.addStock(quantity);
+//            if (product.getPrice() != price) {
+//                product.setPrice(price);
+//            }
+//
+//        } else {
+//            products.add(new Product(productId, price, quantity));
+//        }
     }
 
     void removeProduct(String productId, int quantity) throws InsufficientInventory {
         // you write this class
-        int index = getProductIndex(productId);
-
-        if (index != -1) {
-            if (products.get(index).getQuantity() > quantity) {
-                products.get(index).removeStock(quantity);
-            } else if (products.get(index).getQuantity() == 0) {
-                products.remove(index);
+        Product product = getProduct(productId);
+        if (inStock(productId)) {
+            if (product.getQuantity() > quantity) {
+                product.removeStock(quantity);
+            } else if (product.getQuantity() == 0){
+                products.remove(product);
             } else {
                 throw new InsufficientInventory(
-                        products.get(index).getQuantity(), quantity
+                        product.getQuantity(), quantity
                 );
             }
         } else {
             throw new InsufficientInventory(0, quantity);
         }
+
+//        int index = getProductIndex(productId);
+//
+//        if (index != -1) {
+//            if (products.get(index).getQuantity() > quantity) {
+//                products.get(index).removeStock(quantity);
+//            } else if (products.get(index).getQuantity() == 0) {
+//                products.remove(index);
+//            } else {
+//                throw new InsufficientInventory(
+//                        products.get(index).getQuantity(), quantity
+//                );
+//            }
+//        } else {
+//            throw new InsufficientInventory(0, quantity);
+//        }
     }
 
     Product getProduct(String productId) {
-        int index = getProductIndex(productId);
-        if (index >= 0) {
-            return products.get(index);
-        } else {
-            return null;
+        for (Product p:
+             products) {
+            if (p.getProductId().equals(productId)) {
+                return p;
+            }
         }
+        return null;
+//        int index = getProductIndex(productId);
+//        if (index >= 0) {
+//            return products.get(index);
+//        } else {
+//            return null;
+//        }
     }
 
     String getAllProductNames() {
